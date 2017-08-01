d5e78cf536a52210716385432303c6214daa1f78
anatol-blackforest <anatolblackforest@gmail.com>
2017-08-01 15:07:56 +0300

    index

---
+++ index.js
@@ -0,0 +1,34 @@
+// подключение express и socket.io 
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const escape_html = require('escape-html');
+const path = require('path'); 
+
+const bot = require('./lib/bot'); 
+const render = require('./lib/render'); 
+const connection = require('./lib/connection');
+const config = require('./lib/config');  
+
+let {hints, port} = config;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/html/auth.html')); 
+});
+
+// бот-говорун, говорит каждую минуту (60000 мксек)
+bot(io);
+
+// окно чата для входящего юзера
+app.get('/:id', (req, res) => {
+    render(req, res);
+});
+
+// установка соединения
+io.on('connection', socket => {
+    connection(io, socket);
+}); 
+
+server.listen(port, () => {
+    console.log(`${hints[3]} port`);
+});
