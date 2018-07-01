3e6eaa1908f9ace2abaf6bcaeae6d93f00be8cc8
anatol-blackforest <anatolblackforest@gmail.com>
2018-07-01 14:45:23 +0300

    regg

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const config = require('./lib/config');
 
 let {hints, port, name, clientID, clientSecret, callbackURL} = config;
 
+app.set("twig options", {strict_variables: false});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
 app.use(express.static(path.join(__dirname, 'views', 'script')))
 app.use(express.static(path.join(__dirname, 'views', 'style')))
 app.use(session({keys: ['betelgeuse']}));
@@ -42,7 +46,7 @@ app.get('/auth/facebook', passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/',  failureRedirect: '/' }));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'html', 'auth.html')));
+app.get('/', (req, res) => res.render('auth', {user: req.user}));
 
 // бот-говорун, говорит каждую минуту (60000 мксек)
 bot(io);
@@ -51,7 +55,7 @@ bot(io);
 app.get('/:id', (req, res) => {
     if(!req.isAuthenticated()) return res.redirect("/")
     if(req.params.id !== "favicon.ico"){name.userName = escape_html(req.params.id).substring(0,50)}
-    res.sendFile(path.join(__dirname, 'views', 'html', 'index.html'));
+    res.render('index');
 });
 
 // установка соединения
